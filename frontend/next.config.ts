1c1f8d81d4e65995bde5f0d6d1a192912caac3e9
hope664 <umuhozahoe5@gmail.com>
2026-04-12 22:35:48 +0200

    some changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
+// 
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  allowedDevOrigins: ["192.168.1.66"],
 };
 
 export default nextConfig;
