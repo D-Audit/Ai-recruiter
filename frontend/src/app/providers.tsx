125b390245fb073ac6862d56f2e0fc98e52c64e8
Kayiranga <donjesuskayiranga@gmail.com>
2026-04-20 08:08:29 +0200

    handling refreshing errors

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,22 +1,36 @@
 "use client";
-import { Provider, useSelector } from "react-redux";
-import { store, RootState } from "../store";
+import { useEffect } from "react";
+import { Provider, useDispatch } from "react-redux";
+import { store, AppDispatch } from "../store";
+import { restoreUser } from "../store/slices/authSlice";
 import { Toaster } from "react-hot-toast";
 import FloatingAI from "../components/FloatingAI";
 
-/** Only renders FloatingAI when the user is logged in */
-function AuthGatedFloatingAI() {
-  const token = useSelector((s: RootState) => s.auth.token);
-  if (!token) return null;
-  return <FloatingAI />;
+// Inner component that dispatches restoreUser once on mount
+function AuthRestorer({ children }: { children: React.ReactNode }) {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    // On every page load/refresh: if a token exists in localStorage,
+    // call GET /auth/me to reload the user object into Redux.
+    dispatch(restoreUser());
+  }, [dispatch]);
+
+  return <>{children}</>;
 }
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <Provider store={store}>
-      {children}
-      <Toaster position="top-right" />
-      <AuthGatedFloatingAI />
+      <AuthRestorer>
+        {children}
+        <Toaster position="top-right" />
+        <FloatingAI />
+      </AuthRestorer>
     </Provider>
   );
 }
